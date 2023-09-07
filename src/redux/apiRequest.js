1c094e2b5b0cc327c7f55baa038bbe23d73e8727
thaibao151102 <87563402+thaibao151102@users.noreply.github.com>
2023-09-07 20:57:41 +0700

    edit login api

--- src/redux/apiRequest.js
+++ src/redux/apiRequest.js
@@ -5,7 +5,7 @@ import { loginFailed, loginStart, loginSuccess, logoutFailed, logoutStart, logou
 export const loginUser  = async(user, dispatch, navigate) => {
     dispatch(loginStart())
     try {
-        const res = await axios.post("/auth/login", user)
+        const res = await axios.post("https://auth-server-fmp.vercel.app/auth/login", user)
     
         dispatch(loginSuccess(res.data))
         navigate("/")
@@ -17,7 +17,7 @@ export const loginUser  = async(user, dispatch, navigate) => {
 export const registerUser = async(user, dispatch, navigate) => {
     dispatch(registerStart())
     try {
-        await axios.post("/auth/register", user)
+        await axios.post("https://auth-server-fmp.vercel.app/auth/register", user)
         dispatch(registerSuccess())
         navigate("/login")
     } catch (error) {
